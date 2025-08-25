c893348dfbda609e64289767a43e29d59d67b0b0
Au = Or[79]² <assanisaidelamani@gmail.com>
2025-08-25 19:16:30 +0200

    [EDIT] - register new provider (jwt, role, ratelimit)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProxyController } from './controllers/proxy.controller';
+import { DynamicRateLimitGuard } from './guards/dynamic-rate-limit.guard';
+import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { AdminGuard } from './guards/role.guard';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
-    }),
+    }),  
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProxyController],
+  providers: [
+    AppService, 
+    JwtAuthGuard,
+    AdminGuard,
+    DynamicRateLimitGuard
+  ],
 })
 export class AppModule {}
